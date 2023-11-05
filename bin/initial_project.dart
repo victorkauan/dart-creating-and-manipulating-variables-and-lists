4ab176c0f009dd0c70eb189d152918656170a9df
Victor Kauan <victorkauan.dev@gmail.com>
2023-11-05 14:42:28 -0300

    :sparkles: feat: Learn about integer variables

--- bin/initial_project.dart
+++ bin/initial_project.dart
@@ -1,3 +1,12 @@
 void main(List<String> arguments) {
-  print('Hello, world!');
+  int age = 24;
+  // int age;
+  // age = 24;
+
+  int hexadecimalAge = 0x000018;
+  // int hexadecimalAge;
+  // hexadecimalAge = 0x000018;
+
+  print(age);
+  print(hexadecimalAge);
 }
