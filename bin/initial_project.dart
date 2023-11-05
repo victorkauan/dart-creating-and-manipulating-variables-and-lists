38832ab287bba2aefb571bfe83672b24efba3bfc
Victor Kauan <victorkauan.dev@gmail.com>
2023-11-05 15:24:36 -0300

    :sparkles: feat: Learn about string list variables

--- bin/initial_project.dart
+++ bin/initial_project.dart
@@ -1,5 +1,5 @@
 void main(List<String> arguments) {
-  int age = 24;
+  // int age = 24;
   // int age;
   // age = 24;
 
@@ -7,24 +7,35 @@ void main(List<String> arguments) {
   // int hexadecimalAge;
   // hexadecimalAge = 0x000018;
 
-  double height = 1.7;
+  // double height = 1.7;
   // double doubleAge = 24.0;
 
   // double largeNumber = 780000000;
   // double exponentialLargeNumber = 780e6; // 780 * 10 ^ 6
 
-  bool geek = true;
+  // bool geek = true;
   // bool ageEqualsToHeight = age == height;
   // bool ageEqualsToDoubleAge = age == doubleAge;
 
-  String name = "Victor Kauan";
-  String username = "VK";
-  String greetingSentence = 'I\'m $username but my real name is: $name.\n'
-      'Do I consider myself a geek? $geek.\n'
-      'I\'m $age years old and $height meters tall.';
+  // String name = "Victor Kauan";
+  // String username = "VK";
+  // String greetingSentence = 'I\'m $username but my real name is: $name.\n'
+  //     'Do I consider myself a geek? $geek.\n'
+  //     'I\'m $age years old and $height meters tall.';
 
-  print(name);
-  print(username);
-  print('$name $username');
-  print(greetingSentence);
+  String name1 = "Victor";
+  String name2 = "Felipe";
+  String name3 = "Paula";
+  String name4 = "Mikael";
+  String name5 = "Beatriz";
+
+  List<String> nameList = ["Victor", "Felipe", "Paula", "Mikael", "Beatriz"];
+
+  print('$name1 $name2 $name3 $name4 $name5');
+  print(nameList);
+
+  print(name1);
+  print(nameList[0]);
+
+  print(nameList.length);
 }
