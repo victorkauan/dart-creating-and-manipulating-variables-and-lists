7c0bd3d73fa78e7838777597b9a97d4e4590338b
Victor Kauan <victorkauan.dev@gmail.com>
2023-11-05 15:48:49 -0300

    :sparkles: feat: Learn about const, final and var variables

--- bin/initial_project.dart
+++ bin/initial_project.dart
@@ -1,24 +1,30 @@
 void main(List<String> arguments) {
-  // int age = 24;
+  int age = 24;
   // int age;
   // age = 24;
+  // var age = 24;
 
   // int hexadecimalAge = 0x000018;
   // int hexadecimalAge;
   // hexadecimalAge = 0x000018;
 
-  // double height = 1.7;
+  double height = 1.7;
   // double doubleAge = 24.0;
+  // var height = 1.7;
 
   // double largeNumber = 780000000;
   // double exponentialLargeNumber = 780e6; // 780 * 10 ^ 6
 
-  // bool geek = true;
+  bool geek = true;
+  // var geek = true;
   // bool ageEqualsToHeight = age == height;
   // bool ageEqualsToDoubleAge = age == doubleAge;
 
-  // String name = "Victor Kauan";
-  // String username = "VK";
+  const String name = "Victor Kauan";
+  // final String username = "VK";
+  final String username;
+  username = "VK";
+
   // String greetingSentence = 'I\'m $username but my real name is: $name.\n'
   //     'Do I consider myself a geek? $geek.\n'
   //     'I\'m $age years old and $height meters tall.';
@@ -31,7 +37,7 @@ void main(List<String> arguments) {
   //
   // List<String> nameList = ["Victor", "Felipe", "Paula", "Mikael", "Beatriz"];
 
-  List<dynamic> victor = [24, 1.7, true, 'Victor Kauan', 'VK'];
+  List<dynamic> victor = [age, height, geek, name, username];
 
   String dynamicGreetingSentence = 'I\'m ${victor[4]} but my real name is:'
       ' ${victor[3]}.\n'
