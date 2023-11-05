61736fa3b02f6a84dd214e37abdb241090a2e572
Victor Kauan <victorkauan.dev@gmail.com>
2023-11-05 14:48:11 -0300

    :sparkles: feat: Learn about double variables

--- bin/initial_project.dart
+++ bin/initial_project.dart
@@ -1,12 +1,18 @@
 void main(List<String> arguments) {
-  int age = 24;
+  // int age = 24;
   // int age;
   // age = 24;
 
-  int hexadecimalAge = 0x000018;
+  // int hexadecimalAge = 0x000018;
   // int hexadecimalAge;
   // hexadecimalAge = 0x000018;
 
-  print(age);
-  print(hexadecimalAge);
+  double height = 1.7;
+
+  double largeNumber = 780000000;
+  double exponentialLargeNumber = 780e6; // 780 * 10 ^ 6
+
+  print(height);
+  print(largeNumber);
+  print(exponentialLargeNumber);
 }
