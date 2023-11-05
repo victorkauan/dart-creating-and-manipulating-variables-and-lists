c2e15e1061d8d2c50cb6b31ee6c03e513fbac2fe
Victor Kauan <victorkauan.dev@gmail.com>
2023-11-05 15:05:29 -0300

    :sparkles: feat: Learn about string variables

--- bin/initial_project.dart
+++ bin/initial_project.dart
@@ -8,16 +8,23 @@ void main(List<String> arguments) {
   // hexadecimalAge = 0x000018;
 
   double height = 1.7;
-  double doubleAge = 24.0;
+  // double doubleAge = 24.0;
 
   // double largeNumber = 780000000;
   // double exponentialLargeNumber = 780e6; // 780 * 10 ^ 6
 
   bool geek = true;
-  bool ageEqualsToHeight = age == height;
-  bool ageEqualsToDoubleAge = age == doubleAge;
+  // bool ageEqualsToHeight = age == height;
+  // bool ageEqualsToDoubleAge = age == doubleAge;
 
-  print(geek);
-  print(ageEqualsToHeight);
-  print(ageEqualsToDoubleAge);
+  String name = "Victor Kauan";
+  String username = "VK";
+  String greetingSentence = 'I\'m $username but my real name is: $name.\n'
+      'Do I consider myself a geek? $geek.\n'
+      'I\'m $age years old and $height meters tall.';
+
+  print(name);
+  print(username);
+  print('$name $username');
+  print(greetingSentence);
 }
