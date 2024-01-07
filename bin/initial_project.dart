40f68353d091a736a2f9edd92008470b43d9404f
Victor Kauan <victorkauan.dev@gmail.com>
2024-01-07 17:33:03 -0300

    :sparkles: feat: Learn about for statement

--- bin/initial_project.dart
+++ bin/initial_project.dart
@@ -1,5 +1,5 @@
 void main(List<String> arguments) {
-  int age = 24;
+  // int age = 24;
   // int age;
   // age = 24;
   // var age = 24;
@@ -8,29 +8,29 @@ void main(List<String> arguments) {
   // int hexadecimalAge;
   // hexadecimalAge = 0x000018;
 
-  double height = 1.7;
+  // double height = 1.7;
   // double doubleAge = 24.0;
   // var height = 1.7;
 
   // double largeNumber = 780000000;
   // double exponentialLargeNumber = 780e6; // 780 * 10 ^ 6
 
-  bool geek = true;
+  // bool geek = true;
   // var geek = true;
   // bool ageEqualsToHeight = age == height;
   // bool ageEqualsToDoubleAge = age == doubleAge;
 
-  const String name = "Victor Kauan";
+  // const String name = "Victor Kauan";
   // final String username = "VK";
-  final String username;
-  username = "VK";
-  bool ofLegalAge;
+  // final String username;
+  // username = "VK";
+  // bool ofLegalAge;
 
-  if (age >= 18) {
-    ofLegalAge = true;
-  } else {
-    ofLegalAge = false;
-  }
+  // if (age >= 18) {
+  //   ofLegalAge = true;
+  // } else {
+  //   ofLegalAge = false;
+  // }
 
   // String greetingSentence = 'I\'m $username but my real name is: $name.\n'
   //     'Do I consider myself a geek? $geek.\n'
@@ -44,14 +44,18 @@ void main(List<String> arguments) {
   //
   // List<String> nameList = ["Victor", "Felipe", "Paula", "Mikael", "Beatriz"];
 
-  List<dynamic> victor = [age, height, geek, name, username];
+  // List<dynamic> victor = [age, height, geek, name, username];
+  //
+  // String dynamicGreetingSentence = 'I\'m ${victor[4]} but my real name is:'
+  //     ' ${victor[3]}.\n'
+  //     'Do I consider myself a geek? ${victor[2]}.\n'
+  //     'I\'m ${victor[0]} years old and ${victor[1]} meters tall.\n'
+  //     'Am I of legal age? $ofLegalAge';
+  //
+  // print(victor);
+  // print(dynamicGreetingSentence);
 
-  String dynamicGreetingSentence = 'I\'m ${victor[4]} but my real name is:'
-      ' ${victor[3]}.\n'
-      'Do I consider myself a geek? ${victor[2]}.\n'
-      'I\'m ${victor[0]} years old and ${victor[1]} meters tall.\n'
-      'Am I of legal age? $ofLegalAge';
-
-  print(victor);
-  print(dynamicGreetingSentence);
+  for (int i = 1; i < 5; i++) {
+    print("I completed $i lap(s).");
+  }
 }
